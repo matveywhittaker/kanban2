72b60b781508b1a7ae929882a60fb4d7cb33e6e2
Matvey Whittaker <matvey.whittaker@outlook.com>
2018-11-10 15:30:37 +0000

    Added Router
    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+
+
 
 class App extends Component {
   render() {
